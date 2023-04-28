7762d97b6a6d05dfce730de3d9fb0c69bb369c35
Davide Plozza <dplozza@ethz.ch>
2023-04-28 23:22:54 +0200

    fix dependency

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     author_email='gmargo@mit.edu',
     description='Toolkit for deployment of sim-to-real RL on the Unitree Go1.',
     install_requires=['ml_logger==0.8.117',
-                      'ml_dash==0.3.20',
+                      'ml_dash==0.3.25',
                       'jaynes>=0.9.2',
                       'params-proto==2.10.5',
                       'gym>=0.14.0',
