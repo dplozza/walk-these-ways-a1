80006b2ec3114e5e3c25bd01ec94c0cff9da3ed9
dplozza <dplozza@ethz.ch>
2023-05-22 12:47:32 +0200

    add comments

--- go1_gym/envs/go1/velocity_tracking/__init__.py
+++ go1_gym/envs/go1/velocity_tracking/__init__.py
@@ -1,3 +1,11 @@
+"""
+This module defines a gym environment for tracking the ground truth velocity of the base of a legged robot.
+
+Classes:
+    VelocityTrackingEasyEnv(LeggedRobot): a gym environment for velocity tracking.
+
+"""
+
 from isaacgym import gymutil, gymapi
 import torch
 from params_proto import Meta
@@ -10,6 +18,26 @@ from go1_gym.envs.base.legged_robot_config import Cfg
 class VelocityTrackingEasyEnv(LeggedRobot):
     def __init__(self, sim_device, headless, num_envs=None, prone=False, deploy=False,
                  cfg: Cfg = None, eval_cfg: Cfg = None, initial_dynamics_dict=None, physics_engine="SIM_PHYSX"):
+        """
+        Initializes the VelocityTrackingEasyEnv gym environment.
+
+        Note that foot contacts are binarized.
+        Some indices are hardcoded for the go1 and A1 models, so this environment may not work with other robots.
+
+        Args:
+            sim_device: the device on which to simulate the environment.
+            headless:   a boolean indicating whether to display the simulation window.
+            num_envs:   the number of environments to create (optional).
+            prone:      a boolean indicating whether the robot starts in a prone position (optional).
+            deploy:     a boolean indicating whether to deploy the environment (optional).
+            cfg:        a configuration object for the environment (optional).
+            eval_cfg:   a configuration object for evaluation of the environment (optional).
+            initial_dynamics_dict:  a dictionary of initial dynamics settings for the environment (optional).
+            physics_engine:         the physics engine to use for simulation (default is "SIM_PHYSX").
+
+        Returns:
+            None
+        """
 
         if num_envs is not None:
             cfg.env.num_envs = num_envs
@@ -22,6 +50,8 @@ class VelocityTrackingEasyEnv(LeggedRobot):
     def step(self, actions):
         self.obs_buf, self.privileged_obs_buf, self.rew_buf, self.reset_buf, self.extras = super().step(actions)
 
+        # extract foot positions, with hardcoded indices from go1 or A1 models
+        # The  robot has 1 body and the number of joints is 12 leg joints + 1 base joint
         self.foot_positions = self.rigid_body_state.view(self.num_envs, self.num_bodies, 13)[:, self.feet_indices,
                                0:3]
 
